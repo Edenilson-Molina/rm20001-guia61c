39d2571307946c734bb28dd217f9cc8602441a7b
Edenilson-Molina <90731597+Edenilson-Molina@users.noreply.github.com>
2022-10-21 23:59:19 -0600

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -76,7 +76,8 @@ function deleteProducto(id)
 		})
 		.then(response => response.json())
 		.then(data => {delresult = data;obtenerProductos()});
-}	alert(id);
+	alert(id);
+}
 
 function obtenerProductos() {
 	  fetch('https://api-generator.retool.com/eI9mm6/productos')
